45899c4bce03cc4ae2365be98690ca50806814c4
Johan <johan.hatleskog@gmail.com>
2017-01-15 13:57:22 +0100

    Add script for plotting relative rotations from state

---
+++ plotRelativeRotations.m
@@ -0,0 +1,30 @@
+function plotRelativeRotations(state)
+
+H = reshape(state.poses,[4,4,size(state.poses,2)]);
+N = size(state.poses,2);
+
+twist = zeros(3,N);
+
+for i = 2:N
+    H_W1 = H(:,:,i-1);
+    H_W2 = H(:,:,i);
+    H_12 = H_W1^-1*H_W2;
+    
+    R = H_12(1:3,1:3);
+    omega = logm(R); % Skew matrix
+    
+    twist(:,i) = omega([6,7,2]);
+end    
+    
+figure(6);clf; 
+plot(twist(1,:)*180/pi)
+hold on
+plot(twist(2,:)*180/pi)
+plot(twist(3,:)*180/pi) 
+legend('X','Y','Z');
+title('Rotation vector coords. H_{i-1,i} for each frame i');
+xlabel('Frame number');
+ylabel('rotation coord [deg]');
+ylim([-50 50]);
+
+end
